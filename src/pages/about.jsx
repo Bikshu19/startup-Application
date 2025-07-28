0d6b8ee1ca049b06113383c38289317a7a7cf218
Bikshu19 <bikshutulimelli@gmail.com>
2025-07-28 17:33:53 +0530

    bkk

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,26 +3,14 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/footer";
 import Image from "../components/Image";
 import Aboutsection from "../components/aboutsection";
-<<<<<<< HEAD
 import WhatsAppButton from '../components/whatsapp';
 import ChatWidget from '../components/ChatWidget';
-=======
-import AboutPageImage from "../components/AboutPageImage";
->>>>>>> 2dfff553740fb9f2a209de654fad83bd39e7a1bf
 
 function About() {
   return (
     <div>
         <Navbar />
-<<<<<<< HEAD
         <Image />
-=======
-      </div>
-      <div>
-        <AboutPageImage />
-      </div>
-      <div>
->>>>>>> 2dfff553740fb9f2a209de654fad83bd39e7a1bf
         <Aboutsection />
       <WhatsAppButton/>
         <ChatWidget/>
