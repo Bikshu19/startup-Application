5fa0ae501a898efc2b8f85eb08bb7772a92ee797
YARRACHANDU <yarrachandu7@gmail.com>
2025-07-28 15:01:28 +0530

    chandu

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import Footer from '../components/footer';
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/footer";
+import Image from "../components/Image";
+import Aboutsection from "../components/aboutsection";
 
 function About() {
   return (
     <div>
-        <div>
-            <Navbar/>
-        </div>
-        <Footer/>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Image />
+      </div>
+      <div>
+        <Aboutsection />
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
