10320027e6a08d5bc14e43a82fba8ddb248e5993
YARRACHANDU <yarrachandu7@gmail.com>
2025-07-28 16:16:13 +0530

    chandu

--- src/components/AboutPageImage.jsx
+++ src/components/AboutPageImage.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import HeroText from "./Herotext";
 import HeroTextForAbout from "./HeroTextForAbout";
+import TypingForAbout from "./TypingForAbout";
 
 function AboutPageImage() {
   return (
     <div className="relative w-screen h-screen">
       <img
-        src="/1e.png"
+        src="/1d.jpg"
         alt="Background"
         className="absolute inset-0 w-full h-full object-cover object-center z-0"
       />
       <div className="absolute inset-0 bg-gradient-to-r from-[#ADB2D4] to-transparent z-10 h-full" />
+
       <HeroTextForAbout />
     </div>
   );
