782cbc111d734da5995526f8f4a7f83c9c90e5a8
SulthanPatan07 <sulthanpatan786@gmail.com>
2025-07-29 12:21:21 +0530

    sulthan

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import Navbar from '../components/Navbar';
-import Footer from '../components/footer';
-import WhatsAppButton from '../components/whatsapp';
-import ChatWidget from '../components/ChatWidget';
-import Contacts from '../components/contacts';
-=======
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/footer";
@@ -13,17 +5,12 @@ import WhatsAppButton from "../components/whatsapp";
 import ChatWidget from "../components/ChatWidget";
 
 import ContactPage from "../components/ContactForm";
->>>>>>> d46742fa5f4a4d25d149caba741c051e3b31cbbf
 
 function Contact() {
   return (
     <div>
       <Navbar />
-<<<<<<< HEAD
-      <Contacts />
-=======
       <ContactPage />
->>>>>>> d46742fa5f4a4d25d149caba741c051e3b31cbbf
       <WhatsAppButton />
       <ChatWidget />
       <Footer />
