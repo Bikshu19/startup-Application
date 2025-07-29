5b2de428e3add577c8eea7585c80d30c9d5e93a6
SulthanPatan07 <sulthanpatan786@gmail.com>
2025-07-29 10:38:09 +0530

    hu

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -3,16 +3,18 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/footer';
 import WhatsAppButton from '../components/whatsapp';
 import ChatWidget from '../components/ChatWidget';
+import Contacts from '../components/contacts';
 
 function Contact() {
   return (
     <div>
-        <Navbar/>
-        <WhatsAppButton/>
-        <ChatWidget/>
-        <Footer/>
+      <Navbar />
+      <Contacts />
+      <WhatsAppButton />
+      <ChatWidget />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
