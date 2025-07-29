733091ecec50b8ee2129a65256fefafb536bc9a5
SulthanPatan07 <sulthanpatan786@gmail.com>
2025-07-29 12:23:18 +0530

    hello

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -5,22 +5,16 @@ import WhatsAppButton from "../components/whatsapp";
 import ChatWidget from "../components/ChatWidget";
 
 import ContactPage from "../components/ContactForm";
-<<<<<<< HEAD
-=======
 import ContactCards from "../components/ContactCards";
 import CustomerCare from "../components/CustomerCare";
->>>>>>> c450ec958f3f2e5c94f9d80324ab1a2ac8b7f437
 
 function Contact() {
   return (
     <div>
       <Navbar />
       <ContactPage />
-<<<<<<< HEAD
-=======
       <ContactCards />
       <CustomerCare />
->>>>>>> c450ec958f3f2e5c94f9d80324ab1a2ac8b7f437
       <WhatsAppButton />
       <ChatWidget />
       <Footer />
