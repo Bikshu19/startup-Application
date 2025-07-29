33c7c7cc4e5e2235389c65fbbd1ceb8945d670a6
YARRACHANDU <yarrachandu7@gmail.com>
2025-07-29 10:57:32 +0530

    chandu

--- src/components/AboutPageImage.jsx
+++ src/components/AboutPageImage.jsx
@@ -1,21 +1,19 @@
+// components/Image.jsx
 import React from "react";
 import HeroText from "./Herotext";
-import HeroTextForAbout from "./HeroTextForAbout";
-import TypingForAbout from "./TypingForAbout";
 
-function AboutPageImage() {
+function Image() {
   return (
     <div className="relative w-screen h-screen">
       <img
-        src="/1e.png"
+        src="/12.png"
         alt="Background"
         className="absolute inset-0 w-full h-full object-cover object-center z-0"
       />
       <div className="absolute inset-0 bg-gradient-to-r from-[#ADB2D4] to-transparent z-10 h-full" />
-
-      <HeroTextForAbout />
+      <HeroText />
     </div>
   );
 }
 
-export default AboutPageImage;
+export default Image;
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/footer";
 import WhatsAppButton from "../components/whatsapp";
 import ChatWidget from "../components/ChatWidget";
-import ContactForm from "../components/ContactForm";
+
 import ContactPage from "../components/ContactForm";
 
 function Contact() {
